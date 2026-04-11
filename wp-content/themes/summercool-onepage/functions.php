f1be42f96638993c371cc9e9c5d870119bded9bd
Siriretomiwa <132751656+Siriretomiwa@users.noreply.github.com>
2026-04-11 03:16:12 +0200

    Update functions.php

--- wp-content/themes/summercool-onepage/functions.php
+++ wp-content/themes/summercool-onepage/functions.php
@@ -32,5 +32,6 @@ function summercool_onepage_setup(): void
 {
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
+    add_theme_support('woocommerce');
 }
 add_action('after_setup_theme', 'summercool_onepage_setup');
